0ee38ccac676885fade893234cff1e0977ec81c8
Vivian <1030518209@qq.com>
2017-09-05 20:15:48 +0800

    修复了ui的bug，写了slider的代码

--- LoveFinder/LoveFinder/ViewController.swift
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var gender: UISegmentedControl!
+    
+    @IBOutlet weak var birthday: UIDatePicker!
+    
+    @IBOutlet weak var height: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var haveHouse: UISwitch!
+    
+    @IBOutlet weak var okDidTap: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        name.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeHeight(_ sender: Any) {
+        var slider = sender as! UISlider
+        var h = slider.value
+        var i = Int(h)
+        heightLabel.text = "\(i)cm"
+    }
 
+    
+    @IBAction func okDidTap(_ sender: Any) {
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        name.resignFirstResponder()
+        return true
+    }
 }
 
